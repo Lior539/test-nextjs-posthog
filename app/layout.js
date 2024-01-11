021e5454e3551c7ad7a48eb04bfcc56874e82e8f
Lior <lneuner@gmail.com>
2024-01-11 11:43:40 +0000

    separate component

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,23 @@
 // app/layout.js
+import { PostHogPageview } from './PostHogPageView'
 import './globals.css'
-import { PHProvider, PostHogPageview } from './providers'
+import { PHProvider } from './providers'
 import { Suspense } from 'react'
+
+// import dynamic from 'next/dynamic'
+
+// const PostHogPageview = dynamic(() => import('./PostHogPageView.js'), { ssr: false })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Suspense fallback={<div />}>
-        <PostHogPageview />
-      </Suspense>
+            
       <PHProvider>
+        <Suspense fallback={<div />}>
+        <PostHogPageview /> 
+      </Suspense>
+      
+
         <body>{children}</body>
       </PHProvider>
     </html>
--- app/providers.js
+++ app/providers.js
@@ -2,32 +2,11 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
-import { usePathname, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 
 if (typeof window !== 'undefined') {
   posthog.init("phc_Is2364QKW0QKHbXWVtgh8Yugx8xAvdfL71JmBaKD8mG", {
     api_host: "https://app.posthog.com"
   })
-}
-export function PostHogPageview() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  // Track pageviews
-  useEffect(() => {
-    if (pathname) {
-      let url = window.origin + pathname
-      if (searchParams.toString()) {
-        url = url + `?${searchParams.toString()}`
-      }
-      posthog.capture(
-        '$pageview',
-        {
-          '$current_url': url,
-        }
-      )
-    }
-  }, [pathname, searchParams])
 }
 
 export function PHProvider({ children }) {
