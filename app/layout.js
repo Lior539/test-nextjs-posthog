3945a65e5ba2c6cd950ddae55b558fcce7377c5b
Lior <lneuner@gmail.com>
2024-01-11 10:43:51 +0000

    no suspense

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
 // app/layout.js
 import './globals.css'
 import { PHProvider, PostHogPageview } from './providers'
-import { Suspense } from 'react'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Suspense>
-        <PostHogPageview />
-      </Suspense>
+      <PostHogPageview />
       <PHProvider>
         <body>{children}</body>
       </PHProvider>
